50aa22bf4043b1e08a69440a427ff3ebe9f6a50c
MaiaPiko <maia.mlynczak@gmail.com>
2023-10-24 23:58:37 +0200

    makeWaves!

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
-function generateRandomSinusoidalSVG() {
-    // Calculate the width of the SVG based on the screen width
-    const screenWidth = window.innerWidth;
-    const svgWidth = screenWidth;
-    const svgHeight = 400; // Set a fixed height
-
-    // Create an SVG element
+function waveCreator(color, opacity){
+    const svgWidth = window.innerWidth;
+    const svgHeight = 400;
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
     svg.setAttribute("width", svgWidth);
     svg.setAttribute("height", svgHeight);
-    const amplitude = 50; // Random amplitude
 
-    // const amplitude = Math.random() * 100; // Random amplitude
-    const frequency = 0.01; // Random frequency
-    const phase = Math.random() * 2 * Math.PI; // Random phase
+    const frequency = Math.random() * ((0.02 - 0.009) + 0.009);
+    const amplitude = Math.random() * 80;
+    const phase = Math.random()*10;
 
-    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    let pathData = `M0 ${svgHeight / 2} `;
+    let pathData = `M 0 ${svgHeight/2}`
 
-    for (let x = 0; x < svgWidth; x++) {
-      const y = amplitude * Math.sin(frequency * x + phase) + svgHeight / 2;
-      pathData += `L${x} ${y} `;
+    for (let x=0; x<svgWidth; x++){
+        const y = amplitude * Math.sin(frequency * x + phase) + svgHeight/2;
+        pathData +=  `L ${x} ${y}`;
     }
-    pathData += `L${svgWidth} ${svgHeight} L0 ${svgHeight} Z`;
-
+    
+    pathData += `L ${svgWidth} ${svgHeight} L 0 ${svgHeight} Z`
     
     path.setAttribute("d", pathData);
-    path.setAttribute("fill", "pink");
-    path.setAttribute("stroke", "black");
-
     svg.appendChild(path);
+    const waveContainer = document.getElementById("wave-container");
+    waveContainer.appendChild(svg);
+    path.setAttribute("fill", color);
+    path.setAttribute("opacity", opacity)
 
-    return svg;
-  }
+    // path.setAttribute("stroke", "black");
 
-  // Function to update the SVG element in the container
-  function updateSVG() {
-    const svgContainer = document.getElementById('svg-container');
-    svgContainer.innerHTML = ''; // Clear the previous content
-    const svgElement = generateRandomSinusoidalSVG();
-    svgContainer.appendChild(svgElement);
-  }
+ 
+}
 
-  // Initially display the SVG
-  updateSVG();
+
+// The function takes 'color' and 'opacity' as params. 
+waveCreator("blue", "0.4");
